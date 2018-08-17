479a84f055a46e141259582cb05a05c3827f68cd
Theljani Ahmed <ahmed.theljani@transparency-one.com>
2018-08-17 15:38:40 +0200

    moe tests

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -53,7 +53,6 @@ class Counter extends React.Component {
     }
   }
 
-
   render() {
     return (
       <div className="container">
--- src/components/__tests__/counter.spec.js
+++ src/components/__tests__/counter.spec.js
@@ -6,8 +6,8 @@ import renderer from 'react-test-renderer';
 
 // Components
 import Counter from '../Counter';
-
-
+import InputFied from '../InputField';
+import Button from '../Button';
 
 describe('The counter compponent', () => {
 
@@ -17,7 +17,7 @@ describe('The counter compponent', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('Should Initialise state properly', () => {
+  it('Should initialise state properly', () => {
     const wrapper = shallow(<Counter />);
 
     expect(wrapper.state().counter).toEqual(0);
@@ -28,7 +28,10 @@ describe('The counter compponent', () => {
     const event = {
       preventDefault: () => {}
     };
-    wrapper.instance().onIncrement(event);
+
+    const instance = wrapper.instance();
+
+    instance.onIncrement(event);
     expect(wrapper.state().counter).toBe(1);
   })
 
@@ -37,15 +40,49 @@ describe('The counter compponent', () => {
     const event = {
       preventDefault: () => {}
     };
-    wrapper.instance().onIncrement(event);
-    wrapper.instance().onIncrement(event);
-    wrapper.instance().onDecrement(event);
+    const instance = wrapper.instance();
+
+    instance.onIncrement(event);
+    instance.onIncrement(event);
+    instance.onDecrement(event);
     expect(wrapper.state().counter).toBe(1);
   })
 
   it('Should contains an InputField component as a child', () => {
-    const wrapper = mount(<Counter />);
-    console.log(JSON.stringify(wrapper));
+    const wrapper = mount(<Counter />); 
+    const inputField = wrapper.find(InputFied);
+
+    expect(inputField).toBeDefined();
+    expect(inputField).toHaveLength(1);
+  });
+
+  it('Should contains an InputField component as a child and progarate props properly', () => {
+    const wrapper = mount(<Counter />); 
+    const inputField = wrapper.find(InputFied);
+    expect(inputField).toBeDefined();
+    expect(inputField.props()).toBeDefined();
+    expect(inputField.props().value).toEqual(0);
+    expect(inputField.props().value).toEqual(0);
+    expect(inputField.props().name).toEqual('counter');
+    expect(inputField.props().label).toEqual('Counter');
+    expect(inputField.props().errors).toBeDefined();
+    expect(inputField.props().errors).toHaveLength(0);
+    expect(inputField.props().onChange).toBeDefined();
+    expect(inputField.props().onBlur).toBeDefined();
+    expect(inputField.props().onFocus).toBeDefined();
+  });
+
+  it('Should contains 2 Button components as children', () => {
+    const counterComponent = mount(<Counter />);
+
+    expect(counterComponent).toBeDefined();
+    expect(counterComponent).toMatchSnapshot();
+
+    const buttonComponents = counterComponent.find(<Button />);
+
+    expect(buttonComponents).toBeDefined();
+
+    console.log(buttonComponents.first());
   });
 });
 
